c5e4873a1504cc7bb3f70ad67fbbf3ccfbd7bc15
Sam Roberts <sam@strongloop.com>
2015-04-28 21:28:44 -0700

    Update pm and deploy to multiapp versions

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ var assert = require('assert');
 var debug = require('debug')('strong-start:start');
 var deploy = require('strong-deploy').local;
 var fs = require('fs');
+var path = require('path');
 
 module.exports = start;
 
 function start(app) {
-  debug('app is %j', app);
+  debug('start app %j', app);
 
+  var pkg = path.join(path.resolve(app || '.'), 'package.json');
+  var name = require(pkg).name;
   var pm = new Pm();
 
   pm.start(run);
@@ -21,8 +24,8 @@ function start(app) {
       process.exit(1);
     }
 
-    debug('deploy to %j app %j', pm.url, app);
-    deploy(pm.url, app, 'default', ran);
+    debug('deploy to %j app %j name %j', pm.url, app);
+    deploy(pm.url, name, app, ran);
   }
 
   function ran(err) {
